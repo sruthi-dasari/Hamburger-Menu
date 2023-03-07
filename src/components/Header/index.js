ec2e92e89faf77501c19cb71e31bcf7e4dc26914
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-07 17:32:16 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,84 +1,82 @@
-import {Link} from 'react-router-dom'
-
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {AiFillHome} from 'react-icons/ai'
-import {BsInfoCircleFill} from 'react-icons/bs'
-import {IoMdClose} from 'react-icons/io'
-
-import './index.css'
-
-import Popup from 'reactjs-popup'
-
-import 'reactjs-popup/dist/index.css'
-
-const Header = () => {
-  const overlayStyles = {
-    backgroundColor: '#ffff',
-  }
-
-  return (
-    <div className="header-container">
-      <Link to="/">
-        <button type="button" className="logo-btn">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-            alt="website logo"
-            className="website-logo-img"
-          />
-        </button>
-      </Link>
-      <div className="popup-container">
-        <Popup
-          modal
-          trigger={
-            <button
-              type="button"
-              className="hamburger-btn"
-              data-testid="hamburgerIconButton"
-            >
-              <GiHamburgerMenu className="hamburger-img" />
-            </button>
-          }
-          overlayStyle={overlayStyles}
-          className="popup-content"
-        >
-          {close => (
-            <div className="popup-outer-box">
-              <button
-                type="button"
-                className="trigger-button"
-                data-testid="closeButton"
-                onClick={() => close()}
-              >
-                <IoMdClose className="close-icon" />
-              </button>
-              <ul className="links-box">
-                <Link
-                  to="/"
-                  className="home-about-link"
-                  onClick={() => close()}
-                >
-                  <li className="box-container">
-                    <AiFillHome className="home-about-icon" />
-                    <h1 className="home-about-text">Home</h1>
-                  </li>
-                </Link>
-                <Link
-                  to="/about"
-                  className="home-about-link"
-                  onClick={() => close()}
-                >
-                  <li className="box-container">
-                    <BsInfoCircleFill className="home-about-icon" />
-                    <h1 className="home-about-text">About</h1>
-                  </li>
-                </Link>
-              </ul>
-            </div>
-          )}
-        </Popup>
-      </div>
-    </div>
-  )
+.header-container {
+  border-bottom: solid 0.5px #dcdcdc;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
 }
-export default Header
+
+.logo-btn {
+  background: transparent;
+  border: none;
+}
+
+.website-logo-img {
+  height: 40px;
+  width: 45px;
+}
+
+.hamburger-btn {
+  background: transparent;
+  border: none;
+}
+
+.hamburger-img {
+  height: 35px;
+  width: 35px;
+}
+
+.popup-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.popup-content {
+  border: none !important;
+}
+
+.links-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.box-container {
+  display: flex;
+  align-items: center;
+}
+
+.home-about-icon {
+  height: 50px;
+  width: 50px;
+  /* color: #616e7c; */
+  margin-right: 5px;
+}
+
+.home-about-text {
+  font-size: 40px;
+  font-weight: 500;
+  font-family: 'Roboto';
+  /* color: #616e7c; */
+}
+
+.close-button {
+  background: transparent;
+  border: none;
+  align-self: flex-end;
+}
+
+.popup-outer-box {
+  display: flex;
+  flex-direction: column;
+}
+
+.close-icon {
+  height: 40px;
+  width: 40px;
+}
+
+.home-about-link {
+  text-decoration: none;
+  color: black;
+}
